b4cb557489909e7f454d7ec7824660a8883470d9
Yalalova <yalalova01@gmail.com>
2020-12-05 16:17:16 -0600

    Create GroupCreationTests

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -6,8 +6,6 @@ import org.testng.Assert;
 public class PointTests {
 
 @Test
-
-
   public void testdistance() {
     Point p = new Point(2, 2, 3, 1);
   Assert.assertEquals(p.distance(), 1.4142135623730951);
